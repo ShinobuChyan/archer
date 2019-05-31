61859f023b04234e9f40941e84a2ed824369aa8a
ShinobuChyan <liyao@juxiangfen.com>
2019-05-31 14:02:54 +0800

    remove unused code

--- server-common/src/main/java/com/archer/server/common/util/HttpRequestUtil.java
+++ server-common/src/main/java/com/archer/server/common/util/HttpRequestUtil.java
@@ -1,15 +1,7 @@
 package com.archer.server.common.util;
 
-import com.archer.server.common.exception.RestRuntimeException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.net.URI;
 import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.time.Duration;
-import java.util.Map;
 
 /**
  * 发送HTTP请求的
@@ -18,8 +10,6 @@ import java.util.Map;
  * @since 2018/10/8
  */
 public class HttpRequestUtil {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(HttpRequestUtil.class);
 
     public static final HttpClient CLIENT = HttpClient.newBuilder()
             .connectTimeout(Duration.ofSeconds(60))
